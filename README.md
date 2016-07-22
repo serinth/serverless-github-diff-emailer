24e8ff7acf4aaa7587ee41c661ba50db91f3162c
Tony Truong <Tony Truong>
2016-07-22 23:39:51 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+This generates a unified diff email for GitHub's WebHooks in the Settings page of the repositories.
+It handles the default Push events only.
+
+This is built using [Serverless](http://serverless.com/) which works ontop of AWS API Gateway and Lambda.
+
+# Requirements #
+- AWS Account with privileges to create IAM Roles, Lambda Functions and API Gateway Endpoints
+- AWS Profile Configured
+- NPM and Node.js
+
+# Getting Started #
+1. Install Serverless `npm install serverless -g`
+2. Clone the this repository
+3. Run `serverless init` inside the project directory
+4. `cd` into the lambda function github-push-notification folder and run `npm install`
+5. Modify the github-push-notification/config.json file to match your requirements
+6. Deploy using `serverless dash deploy`
+7. Log onto the AWS Console and go to API Gateway. Grab the ARN URL for the **github-push-notification** post endpoint and put that into the GitHub Webhook Settings page 
+
+If you modify the template and want to just update the function simply run:
+`serverless function deploy`
---
+++ github-push-notification/handler.js
@@ -0,0 +1,152 @@
+var https = require('https');
+var crypto = require('crypto');
+var config = require('./config.json');
+var parse = require('diff-parse');
+var nodemailer = require('nodemailer');
+var escape = require('escape-html');
+
+module.exports.handler = (event, context, callback) => {
+    event.headers=JSON.parse(event.headers);
+
+    var githubHmac = event.headers['X-Hub-Signature'].replace('sha1=','');
+    var lambdaHmac = crypto.createHmac('sha1',config.github.secret).update(JSON.stringify(event.body)).digest('hex');
+
+    if(githubHmac !== lambdaHmac){
+        context.fail("Invalid GitHub Secret",null);
+    } else { 
+
+        var commits = event.body.commits;
+
+        commits.forEach(function(e,i,a){
+            getDiffFromGitHub(e.url)
+            .then((diff)=>{
+                var files = parse(diff);                
+                var email = '<html><body>';
+                
+                email += 'Branch: ' + event.body.ref + '<br/>';
+                email += 'Home: <a href="' + event.body.repository.html_url + '">' + event.body.repository.html_url + '</a><br/>';
+                email += 'Commit: <a href="' + e.url + '">' + e.id + '</a><br/>';
+                email += 'Author: <a href="mailto:' + e.author.email + '">' + e.author.name + '</a><br/>';
+                email += 'Compare: <a href="' + event.body.compare + '">' + event.body.compare + '</a><br/>';
+                
+
+                files.forEach(function(file){
+                
+                    email += '<hr>' + file.to + '</hr>'; //file modified name
+                    email += '<table style="font-family: monospace, \'Courier New\', Courier; font-size: 12px; margin: 0;">';
+
+                    for(var i = 0; i < file.lines.length; i++){
+                        //Build HTML EMAIL
+                        var change = file.lines[i];
+
+                        if(change.add){
+                            email += '<tr style="background-color:#ddffdd;">';
+                        } else if (change.del){
+                            email += '<tr style="background-color:#ffdddd;">';
+                        } else if (change.normal){
+                            email += '<tr">';
+                        }                
+
+                        if(change.add || change.del || change.normal){
+                            email += '<td style="border-right:1px solid lightgray;">';
+                            email += (change.normal) ? change.ln2 : change.ln;
+                            email += '</td>';   
+                            //Deal with tabs and spaces for emails
+                            var content = escape(change.content);
+                            content = content.replace(/\t/g,'\u00a0 \u00a0'); 
+                            content = content.replace(/ /g, '\u00a0');   
+
+                            if(change.add === true){
+                                email += '<td>+' + content + '</td>';
+                            } else if (change.del){
+                                email += '<td>-' + content + '</td>';   
+                            } else {
+                                email += '<td>' + content + '</td>';   
+                            }
+                            email += '</tr>';
+                        }
+                            
+                        
+                    };
+                                   
+                    email+='</table><br/>';
+                });
+
+                email+='</body></html>';
+                return Promise.resolve({email:email,commit:e});
+            })
+            .then((c)=>{
+                var subject = '[' + event.body.repository.full_name + '] ' + c.commit.id.substring(0,6) + ': ' + c.commit.message;
+                var from = c.commit.author.name + '<' + c.commit.author.email + '>';                
+                sendMail(c.email, subject, from);
+            })
+            .catch((err)=>{
+                callback(err);
+            });
+        });
+
+    }
+};
+
+
+function getDiffFromGitHub(url){
+    var promise = new Promise((resolve,reject)=>{
+        https.get(url + '.diff',(res)=>{
+            var data='';
+            res.on('data',(chunk)=>{
+                data+=chunk;
+            }); 
+            res.on('end',()=>{
+                resolve(data);
+            });
+            
+            res.on('err',(err)=>{
+                console.log("HTTP GET ERROR GITHUB URL:" + err);
+                reject(err); 
+            })
+            .on('error',(err)=>{
+                console.log(err);
+                reject(err);
+            });
+        });
+    });
+
+    return promise;
+};
+
+function sendMail(email, subject, from){ 
+
+    var auth = {
+        user: config.mailConfig.username,
+        pass: config.mailConfig.password
+    };
+
+    var options = {
+        host:config.mailConfig.host,
+        port: config.mailConfig.port,
+        secure: config.mailConfig.secure,
+        requireTLS: config.mailConfig.requireTLS      
+    };
+
+    if(auth.user.length > 0 && auth.pass.length > 0){
+        options['auth'] = auth;
+        options['authMethod'] = config.mailConfig.authMethod;
+    }
+    
+    var transporter = nodemailer.createTransport(options);
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: from, 
+        to: config.mailConfig.to, // can be a command separated list of receivers
+        subject: subject, 
+        html: email 
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error) console.log(error);
+        
+        console.log('Message sent: ' + info.response);
+        transporter.close(); 
+    });
+};
